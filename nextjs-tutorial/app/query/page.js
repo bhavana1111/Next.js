5d8c19343294a7a1c3d12b0881e5117303edc3b5
bhavana1111 <bhavanakondeti2000@gmail.com>
2024-04-13 15:23:59 -0500

    Added Dispaly action and Prisma

--- nextjs-tutorial/app/query/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function QueryPage() {
-  return <div className="text-7xl">Query</div>;
-}
-
-export default QueryPage;
--- nextjs-tutorial/app/tasks/page.js
+++ nextjs-tutorial/app/tasks/page.js
@@ -1,7 +1,14 @@
 import React from "react";
+import TaskForm from "@/components/TaskForm";
+import TaskList from "@/components/TaskList";
 
 function TasksPage() {
-  return <div className="text-7xl">Tasks</div>;
+  return (
+    <div className="max-w-lg">
+      <TaskForm />
+      <TaskList />
+    </div>
+  );
 }
 
 export default TasksPage;
