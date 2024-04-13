2f89d748a78604f7417ff1f17ced9683e0b77fee
bhavana1111 <bhavanakondeti2000@gmail.com>
2024-04-13 13:31:49 -0500

    Added Responsive Image and made prisma setup

--- nextjs-tutorial/app/drinks/[id]/page.js
+++ nextjs-tutorial/app/drinks/[id]/page.js
@@ -1,8 +1,31 @@
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-const SingleDrinkPage = ({ params }) => {
-  console.log(params.id);
-  return <div>Single Drink Page</div>;
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+const getSingleDrink = async (id) => {
+  const res = await fetch(`${url}${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch a Drink");
+  }
+  return res.json();
+};
+
+const SingleDrinkPage = async ({ params }) => {
+  const data = await getSingleDrink(params.id);
+  const title = data?.drinks[0]?.strDrink;
+  const imgSrc = data?.drinks[0]?.strDrinkThumb;
+  console.log(title, imgSrc);
+  return (
+    <div>
+      <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
+        Back to Drinks
+      </Link>
+
+      <h1 className="text-4xl mb-8">{title}</h1>
+      <Image src={imgSrc} width={300} height={300} />
+    </div>
+  );
 };
 
 export default SingleDrinkPage;
