9d19b7dbd80d69640d0cec58c2486615717ff402
bhavana1111 <bhavanakondeti2000@gmail.com>
2024-04-18 16:46:54 -0500

    Added React query

--- tour-manager/components/SidebarHeader.jsx
+++ tour-manager/components/SidebarHeader.jsx
@@ -5,7 +5,7 @@ const SidebarHeader = () => {
   return (
     <div className="flex item-center mb-4 gap-4 px-4">
       <SiOpenaigym className="w-10 h-10 text-primary" />
-      <h2 className="text-xl font-extrabold text-primary">Tour Guide</h2>
+      <h2 className="text-xl font-extrabold text-primary ">Tour Guide</h2>
       <ThemeToggle />
     </div>
   );
